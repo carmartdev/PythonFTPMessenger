bf446bb8fba6ffe14dd344aa6d2d2d1ffdc357d0
Carmartdev <amir8553mohammad@gmail.com>
2023-09-14 16:16:14 +0330

    made it

---
+++ main.py
@@ -0,0 +1,51 @@
+import ftplib
+
+# Fill Required Information
+HOSTNAME = "ftpupload.net"
+USERNAME = "if0_34578425"
+PASSWORD = "psFJtLlF4L"
+UPLOAD_FOLDER = 'htdocs/messenger'
+
+# Inputs
+username = input('Enter your name: ')
+message = input('Enter your message: ')
+
+# Vars
+upload_file = username + message + '.txt'
+
+try:
+    # Connect FTP Server
+    ftp_server = ftplib.FTP(HOSTNAME, USERNAME, PASSWORD)
+    # force UTF-8 encoding
+    ftp_server.encoding = "utf-8"
+except:
+    print("ERR: Can't connect to server")
+
+
+def upload(folder_path, filename):
+
+    # Moving to the folder
+    try:
+        ftp_server.cwd(folder_path)
+    except ftplib.error_perm:
+        print('ERR: Folder does not exist')
+        return
+
+    # Read file in binary mode
+    with open(filename, "rb") as file:
+        # Command for Uploading the file "STOR filename"
+        ftp_server.storbinary(f"STOR {filename}", file)
+
+
+def create_message_file(username, message):
+    try:
+        with open(username + message + '.txt', 'w') as f:
+            f.write(message)
+    except FileNotFoundError:
+        print("The directory not found does not exist")
+
+create_message_file(username, message)
+upload(UPLOAD_FOLDER, upload_file)
+
+# Get list of files
+ftp_server.dir()
